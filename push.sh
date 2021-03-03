58199bd5f0c75ada304c273c2b4466b65f4e195b
Thomas Hintermeier <hintermeier-t@protonmail.com>
2021-03-03 18:06:21 +0100

    changing push script

--- push.sh
+++ push.sh
@@ -1,30 +1,14 @@
 #!/bin/sh
 
-setup_git() {
-  git config --global user.email "hintermeier-t@protonmail.com"
-  git config --global user.name "Thomas Hintermeier"
-}
+git config --global user.email "hintermeier-t@protonmail.com"
+git config --global user.name "Thomas Hintermeier"
+git checkout master
+git add *
+git commit -m "Travis update: (Build $TRAVIS_BUILD_NUMBER)"
+# Remove existing "origin"
+git remote rm origin
+# Add new "origin" with access token in the git URL for authentication
+git remote add origin https://${GH_TOKEN}@github.com//hintermeier-t/projet_13_ALEA.git >
+git push origin master --quiet
 
-commit_files() {
-  git checkout master
-  # Current month and year, e.g: Apr 2018
-  dateAndMonth=`date "+%b %Y"`
-  git add *
-  # Create a new commit with a custom build message
-  # with "[skip ci]" to avoid a build loop
-  # and Travis build number for reference
-  git commit -m "Travis update: $dateAndMonth (Build $TRAVIS_BUILD_NUMBER)" -m "[skip ci]"
-}
-
-upload_files() {
-  # Remove existing "origin"
-  git remote rm origin
-  # Add new "origin" with access token in the git URL for authentication
-  git remote add origin https://${GH_TOKEN}@github.com//hintermeier-t/projet_13_ALEA.git > /dev/null 2>&1
-  git push origin master --quiet
-}
-
-setup_git
-commit_files
-upload_files
 
