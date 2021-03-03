bcf481ec3809d21d1ab9a90cf57105c395ee1e2a
Thomas Hintermeier <hintermeier-t@protonmail.com>
2021-03-03 17:15:11 +0100

    push script modification : commit command

--- push.sh
+++ push.sh
@@ -5,7 +5,7 @@ setup_git() {
   git config --global user.name "Thomas Hintermeier"
 }
 
-commit_country_json_files() {
+commit_files() {
   git checkout master
   # Current month and year, e.g: Apr 2018
   dateAndMonth=`date "+%b %Y"`
@@ -20,12 +20,11 @@ upload_files() {
   # Remove existing "origin"
   git remote rm origin
   # Add new "origin" with access token in the git URL for authentication
-  git remote add origin https://hintermeier-t:${GH_TOKEN}@github.com//hintermeier-t/projet_13_ALEA.git > /dev/null 2>&1
+  git remote add origin https://${GH_TOKEN}@github.com//hintermeier-t/projet_13_ALEA.git > /dev/null 2>&1
   git push origin master --quiet
 }
 
 setup_git
-
-commit_country_json_files
+commit_files
 upload_files
 
