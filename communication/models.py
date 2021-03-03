bcf72953e63e5970f3b813ad77b85d0839b30459
Thomas Hintermeier <hintermeier-t@protonmail.com>
2021-03-03 16:48:15 +0100

    Base templates written

--- communication/models.py
+++ communication/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from authentication.models import Employee
 
 # Create your models here.
+class Message (models.Model):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, null=True)
+    content = models.CharField(max_length=500)
+    date = models.DateTimeField()
--- schedule/models.py
+++ schedule/models.py
@@ -1,5 +1,11 @@
 from django.db import models
-
+from authentication.models import Employee
+from management.models import Plot
 # Create your models here.
 class Event (models.Model):
-    
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, null=False)
+    plot = models.ForeignKey(Plot, on_delete=models.CASCADE, null=False)
+    day = models.CharField(max_length=10)
+    start = models.TimeField()
+    end = models.TimeField()
+    occupation = models.CharField(max_length=100, null=False)
